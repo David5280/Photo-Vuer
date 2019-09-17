6f5d3eca69ee5e16f96e235c4661c7b7ff81c03d
David Engel <Davidjengel2012@gmail.com>
2019-09-16 20:50:40 -0600

    Add default stlying

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div id="app">
-    
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
   components: {
@@ -16,12 +14,14 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-seriff;
+    line-height: 1.4;
+  }
 </style>
