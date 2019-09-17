0476ae683bf81dbab1462a01a5c829f80a6cc672
David Engel <Davidjengel2012@gmail.com>
2019-09-17 12:08:30 -0600

    Add search box, state, and handleSubmit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,36 @@
     <h1>
       PHOTO-VUER
     </h1>
+    <form 
+      class='search-box'
+      @submit.prevent='handleSubmit'
+    >
+      <input 
+        type='text' 
+        placeholder='Search...'
+        class='form-input'
+        v-model='search.query'
+      />
+      <button class='form-input'>Search</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data() {
+    return {
+      search: {
+        query: '',
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log('handleSubmit')
+    }
+  }
 }
 </script>
 
@@ -16,11 +40,20 @@ export default {
   .header {
     display: grid;
     grid-template-columns: 20% 60% 20%;
+    border-bottom: 2px solid black;
   }
   h1 {
     text-align: center;
     grid-column-start: 2;
     grid-column-end: 3;
     font-size: 3rem;
+  }
+  form {
+    margin: auto;
+    grid-column-start: 3;
+    grid-column-end: 4;
+  }
+  .form-input {
+    height: 35px;
   }
 </style>
--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -26,5 +26,6 @@ export default {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+    margin-top: 25px;
   }
 </style>
